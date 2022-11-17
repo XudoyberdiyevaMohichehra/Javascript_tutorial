373fba10a21b1a89b339d148604f5bd8886957b1
Mohichehra <xudoyberdiyevem25@gmail.com>
2022-11-17 11:36:22 +0300

    If else, Ternary operator, Nullish

---
+++ 14.11/EX.14.11.22.js
@@ -0,0 +1,118 @@
+// IF ELSE  ---  turli shartlarga asoslangan turli harakatlarni bajarish uchun ishlatiladi.
+// 1.shart natija kursatishi uchun doimo truega teng bulishi kerak
+
+
+// juft va toq sonlar sistemasi
+
+// let num = 12;
+
+//  if(num == 0)
+//     console.log('NOL');
+//  else if(num % 2 == 0) 
+//     console.log('juft son');
+//  else
+//     console.log('toq son');
+
+
+// va gulli qavslarning ishlatilmasligi = log qilganimizda bitta qatorga sigsa qavs qilmasak ham buladi
+
+
+// let num = 12;
+
+//  if(num == 0)
+//     console.log('NOL');
+//  else if(num % 2 == 0) 
+//     console.log('juft son');
+//  else
+//     console.log('toq son');
+
+
+
+
+
+
+
+
+
+// if else da tugri ketme ketlik  
+
+
+// ball- sistemasi.
+// ball - 170 dan balandi grant asosida talabalikka tavsiya etilsin.
+// ball - 100 dan balandi kontrakt asosida talabalikka tavsiya etilsin.
+// ball - 56 dan balandi super kontrakt asosida talabalikka tavsiya etilsin.
+// ball - 56 dan pasi  talabalikka tavsiya etilmasin.
+
+// let a = 57;
+
+// if(a <= 56)
+//     console.log('talabalikka tavsiya etilmasin.');
+// else if(a <= 100)
+//     console.log('super kontrakt asosida talabalikka tavsiya etilsin.');
+// else if(a <= 170)
+//     console.log('kontrakt asosida talabalikka tavsiya etilsin.');
+// else if (a <= 189) 
+//     console.log('grant asosida talabalikka tavsiya etilsin.');
+
+
+
+// let ball = 120;
+
+// if (ball <= 56) {
+//     console.log('talabalikka tavsiya etilmasin');
+// }else if(ball <= 189 && ball > 170){
+//     console.log(' grant asosida talabalikka tavsiya etilsin.');
+// }else if(ball <= 100 && ball > 57){
+//     console.log('super kontrakt asosida talabalikka tavsiya etilsin.');
+// }else if(ball <= 170 && ball > 100){
+//     console.log(' kontrakt asosida talabalikka tavsiya etilsin.');
+// }
+
+
+
+
+
+
+
+
+
+
+// TERNARY OPERATORS== ? : --- Ko'pincha if, else ning ikkitalik holatini soddaroq ko'rinishda ifodalash uchun ishlatiladi. 
+// true/false ni tekshirish uchun ishlatish tavsiya qilinadi.
+
+// let num = 21;
+
+// num % 2 == 0 ? console.log('juft son') : console.log('toq son');
+
+//  if(num == 0)
+//     console.log('NOL');
+//  else if(num % 2 == 0) 
+//     console.log('juft son');
+//  else
+//     console.log('toq son');
+
+
+
+
+
+
+
+
+
+
+
+
+// Nullish ?? -> null undefined
+// OR || -> null undefined, 0 , false, NaN, "",  0n
+
+// console.log(false || 1);
+// console.log(false ?? 1);
+
+// console.log(NaN || 1);
+// console.log(NaN ?? 1);
+
+// console.log(null || 1);
+// console.log(null ?? 1);
+// console.log(undefined || 1);
+// console.log(undefined ?? 1);
+
