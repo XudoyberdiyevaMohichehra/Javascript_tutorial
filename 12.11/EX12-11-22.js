b19f438820527e7ed29919533740822b4f40caf6
Mohichehra <xudoyberdiyevem25@gmail.com>
2022-11-17 11:29:05 +0300

    Type conversation and logical operators

---
+++ 12.11/EX12-11-22.js
@@ -0,0 +1,65 @@
+// TYPE CONVERSIONS === bir typedan boshqa typega o'zgarishi
+
+//   let a = "44";
+//   console.log(+a);
+
+// console.log("" + 1 + 0);  => type string(+ borligi uchun) javob 10 chunki  stringa aylangani uchun xuddi matn kabi qushilib qoldi
+// console.log("" - 1 + 0);  => type number (- borligi uchun) javob -1 chunki string bush.
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log("2" * "3");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// COMPARISONS ----TAQQOSLASH 
+// = -- uzgaruvchilarga qiymat biriktirish uchun.
+//  == ---tenglik   =! -- teng emas
+//  === --- qatiy tenglik deb ataladi. malumotlarni typeni ham taqqoslaydi.  ==!
+//   < -- kichik belgisi
+//    > -- katta belgisi
+//  ! -- emas
+
+// let a = 5;
+// let b = "8";
+// console.log(a == !b);
+
+
+
+
+
+
+
+
+// LOGICAL OPERATORS===  ||-or,  &&--and
+
+let olma = false;
+let banan = false;
+let anor = false;
+let gilos = true;
+console.log(olma && banan && anor && gilos);
+
+//   ||--or koplab false bulib 1 ta true bulsa ham true chiqaradi.
+
+// && -- and koplab true bulib bitta false bulsa ham false chiqaradi.
