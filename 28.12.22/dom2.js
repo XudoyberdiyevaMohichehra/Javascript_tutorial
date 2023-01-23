5d88b1a608c4b204d0db9bcd72489697ef536a3a
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-01-23 15:19:43 +0300

    DOM2 | 28 Dars

---
+++ 28.12.22/dom2.js
@@ -0,0 +1,51 @@
+// DOM2
+// attribute manipulation
+let parent = document.querySelector('.parent')
+
+// console.log(parent.hasAttribute('id'));
+// console.log(parent.getAttribute('class'));
+// console.log(parent.setAttribute('id', 'border'));
+
+// Create Element
+let div =  document.createElement('div')
+let text =  document.createTextNode('hello ')
+
+// div.innerHTML = text
+// div.setAttribute('id', 'border')
+// div.append(text)
+// console.log(div);
+// parent.before(div)
+// div.remove()
+// parent.after(div)
+// parent.prepend(div)
+// parent.append(div)
+// div = parent.cloneNode(true)
+// console.log(div.hasAttribute('class'));
+
+// div.style.height = '200px'
+// div.style.fontFamily = 'arial'
+
+// div.style.cssText = `
+//  background : red;
+//  padding: 20px;
+//  font-size: 20px;
+//  color: white
+// `
+
+// console.log(parent.offsetHeight);
+// console.log(parent.offsetWidth);
+// console.log(parent.offsetLeft);
+// console.log(parent.offsetTop);
+// console.log(parent.clientWidth);
+
+
+
+// console.log(parent.scrollHeight);
+// console.log(parent.scrollWidth);
+
+// function expand() {
+//     parent.style.height = `${parent.scrollHeight}px`
+// }
+// expand()
+
+
